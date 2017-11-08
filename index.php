6a5f1cd114612016cc4c5f5a03354e96bc407472
vittal <vittal@codeworksltd.co.uk>
2017-11-08 19:47:24 +0530

    Convert to html

--- index.php
+++ index.php
@@ -444,7 +444,7 @@
 </div>
 </div>
 </div>
-<?php include "preloader.php" ?>
+<div class="pre_loader"></div>
 <script type=text/javascript src=js/1.11.3_jquery.min.js></script>
 <script type=text/javascript src=js/jquery.easing.1.3.js></script>
 <script type=text/javascript src=js/bootstrap.min.js></script>
